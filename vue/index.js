1b1fd5edd624a65cec6b138f5512c35322489d0f
LiuKun <308880147@qq.com>
2021-11-11 10:21:12 +0800

    移除滚动条插件、移除临时md文件

--- vue/index.js
+++ vue/index.js
@@ -5,14 +5,7 @@ import Vue from 'vue'
 import tablePage from './src/components'
 
 import sticky from 'vuejs-sticky-directive'
-
-import OverlayScrollbars from 'overlayscrollbars'
-import { OverlayScrollbarsPlugin } from 'overlayscrollbars-vue'
-
 Vue.use(sticky)
-
-Vue.use(OverlayScrollbarsPlugin)
-Vue.use(OverlayScrollbars)
 
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.component('tablePage', tablePage)
--- vue/src/main.js
+++ vue/src/main.js
@@ -2,15 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import 'overlayscrollbars/css/OverlayScrollbars.css'
-import OverlayScrollbars from 'overlayscrollbars'
-import { OverlayScrollbarsPlugin } from 'overlayscrollbars-vue'
 import sticky from 'vuejs-sticky-directive'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-
-Vue.use(OverlayScrollbarsPlugin)
-Vue.use(OverlayScrollbars)
 
 Vue.use(ElementUI)
 Vue.use(sticky)
